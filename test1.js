af294960ff55da659c7a710ec09751e52c7304f7
LdKhanh <khanhleemtp>
2020-09-02 14:42:19 +0700

    fix

--- test1.js
+++ test1.js
@@ -3,6 +3,7 @@ const _ = require('lodash')
 const data = []
 const fomula = ['+', '-', '*', '/']
 
+
 //Cell { key, value }
 function Cell(key, value) {
     return {key: key, value: value}    
@@ -10,16 +11,37 @@ function Cell(key, value) {
 
 // init input
 let cell1 = new Cell('A1', '6');
-let cell2 = new Cell('A2', 'A1 5 *')
-let cell3 = new Cell('B1', 'B2 2 +')
-let cell4 = new Cell('B2', '9')
-let cell5 = new Cell('B3', 'B1 2 +')
+let cell2 = new Cell('A2', 'A1 5 * B2 7 * + 2 *')
+let cell3 = new Cell('B4', 'B2 2 + A2 - 5 *')
+let cell4 = new Cell('B2', '9 3 / 6 + 4 * 2 +')
+let cell5 = new Cell('B3', 'B4 2 + A2 * 5')
 
 data.push(cell1)
 data.push(cell2)
 data.push(cell3)
 data.push(cell4)
 data.push(cell5)
+
+console.log('input', data);
+
+
+// sort 
+
+// regex
+const reA = /[^a-zA-Z]/;
+const reN = /[^0-9]/;
+
+function sortAlphaNum(a, b) {
+  const aA = a.replace(reA, "");
+  const bA = b.replace(reA, "");
+  if (aA === bA) {
+    const aN = parseInt(a.replace(reN, ""), 10);
+    const bN = parseInt(b.replace(reN, ""), 10);
+    return aN === bN ? 0 : aN > bN ? 1 : -1;
+  } else {
+    return aA > bA ? 1 : -1;
+  }
+}
 
 
 // caculate postfix
@@ -30,15 +52,15 @@ const postfix = (arr) => {
            let x1 = parseFloat(arrayOperants.pop());
            let x2 = parseFloat(arrayOperants.pop());
            if(x === '*') {
-               arrayOperants.push(x1 * x2)
+               arrayOperants.push(x2 * x1)
            }
            else if(x === '+') {
-               arrayOperants.push(x1 + x2)
+               arrayOperants.push(x2 + x1)
            }
            else if(x === '-') {
-               arrayOperants.push(x1 - x2)
+               arrayOperants.push(x2 - x1)
            } else if(x === '/') {
-               arrayOperants.push(x1 / x2)
+               arrayOperants.push(x2 / x1)
            }
         }
         else {
@@ -65,13 +87,30 @@ const recursiveCell = (origin, item) => {
 }
 
 // output
+const ouput = [];
+
 let result = _.map(data, item =>{   
     if(!isNaN(item.value)) return item;
-
     return recursiveCell(data, item);
 })
 
+// console.log( 'result: ', result);
 
-console.log( 'result: ', result);
+let sortKeys =_.map(result, item => item.key).sort(sortAlphaNum);
+// console.log('sortKeys', sortKeys);
+
+
+for (let i in sortKeys) {
+     _.forEach(result, item => {
+        if(sortKeys[i] === item.key ) {
+            ouput.push(new Cell(sortKeys[i], item.value));
+        }
+    })
+}
+
+console.log('output', ouput);
+
+
+
 
 
